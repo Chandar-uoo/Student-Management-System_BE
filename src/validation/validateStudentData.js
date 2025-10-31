923cf17172f31c9f8d7e858eae5138407458a1f5
Chandar-uoo <chandru666@vgmail.com>
2025-10-31 18:37:00 +0530

    feat : implement student data validation

---
+++ src/validation/validateStudentData.js
@@ -0,0 +1,31 @@
+ const studentSchema = require("../model/studentSchema");
+ const { default: mongoose } = require("mongoose");
+const AppError = require("./AppError");
+
+exports.validateStudentDetails = async ({fullName,email,rollNumber}) => {
+ 
+  // Prepare query to check duplicates
+  const existingStudent = await studentSchema.findOne({
+    $or: [
+      { email },
+      { rollNumber },
+      { fullName },
+    ],
+  });
+
+  if (existingStudent) {
+    let duplicateField = "";
+
+    if (existingStudent.email === email) {
+      duplicateField = "email";
+    } else if (existingStudent.rollNumber === rollNumber) {
+      duplicateField = "roll number";
+    } else if (existingStudent.fullName === fullName) {
+      duplicateField = "name";
+    }
+
+    throw new AppError(`Student with this ${duplicateField} already exists`, 400);
+  }
+
+
+}
