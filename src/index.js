977237aaaa6d617d390e4ce94487708086f255c8
Chandar-uoo <chandru666@vgmail.com>
2025-10-31 18:39:17 +0530

    feat: reorganize imports and enhance CORS configuration for improved routing

--- src/index.js
+++ src/index.js
@@ -3,23 +3,26 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-
-
-
-
-
-
-
+const {authRouter} =  require('./routes/authRouter')
+const {studentRouter} = require("./routes/studentRoutes");
+const {uploadRouter} = require("./routes/uploadRouter")
+const path = require("path");
 // cors 
 app.use(cors({
   origin: 'http://localhost:5173',
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 app.use(express.json());
 app.use(cookieParser())
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
-
-
+// Routes
+app.use("/api", uploadRouter); 
+app.use("/api/auth", authRouter);
+app.use("/api/student", studentRouter);
 
 
 
