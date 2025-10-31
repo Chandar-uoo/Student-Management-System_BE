a4789b22320e950addb7fe254e46443e77958330
Chandar-uoo <chandru666@vgmail.com>
2025-10-31 18:37:51 +0530

    feat implement reusable try catch bloack

---
+++ src/utils/catchAsync.js
@@ -0,0 +1,10 @@
+module.exports=(controller)=>{
+    return async (req,res,next)=>{
+        try{
+            await controller(req,res,next);
+        }
+        catch(error){
+            next(error);
+        }
+    }
+}
